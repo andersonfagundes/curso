7def92c8f5ea870e320499c5bcdaef2804c9fb04
Anderson <anderson.fagundes4@gmail.com>
2023-04-29 01:04:37 +0100

    Adicionado o construtor e getter e setter na classe Turma

--- src/main/java/br/edu/infnet/Turma.java
+++ src/main/java/br/edu/infnet/Turma.java
@@ -6,4 +6,42 @@ public class Turma {
     private Turno turno;
     private Professor professor;
 
+    public Turma(String codigo, String nivelIngles, Turno turno, Professor professor) {
+        this.codigo = codigo;
+        this.nivelIngles = nivelIngles;
+        this.turno = turno;
+        this.professor = professor;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNivelIngles() {
+        return nivelIngles;
+    }
+
+    public void setNivelIngles(String nivelIngles) {
+        this.nivelIngles = nivelIngles;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
 }
