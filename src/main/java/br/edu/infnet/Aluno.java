4e75bf190b1a317c9bb00f82e8fd4d5935d3c6eb
Anderson <anderson.fagundes4@gmail.com>
2023-04-29 01:06:18 +0100

    Criado a classe Aluno

---
+++ src/main/java/br/edu/infnet/Aluno.java
@@ -0,0 +1,47 @@
+package br.edu.infnet;
+
+public class Aluno {
+    private String nome;
+    private String telefone;
+    private String cpf;
+    private Endereco endereco;
+
+    public Aluno(String nome, String telefone, String cpf, Endereco endereco) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
